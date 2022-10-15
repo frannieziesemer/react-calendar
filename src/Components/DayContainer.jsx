30b760d651b000fc3e4d3b45f9556a57713c1efd
Frannie Ziesemer <frances.ziesemer@gmail.com>
2022-10-15 17:20:04 +0200

    Add padding so first day of month lands on the correct day

--- src/Components/DayContainer.jsx
+++ src/Components/DayContainer.jsx
@@ -3,17 +3,30 @@ import { useState } from "react";
 import { Day } from "./Day";
 
 export const DayContainer = ({ monthNumber, year }) => {
-  //new Date(year, monthNumber, 1) returns the first of the current month
-  //new Date(year, monthNumber, 0) returns the last day of the previous month
-  //new Date(year, monthNumber + 1, 0) returns the last day of the current month
-
+  const firstDayofCurrentMonth = new Date(year, monthNumber, 1);
+  const paddingDays = firstDayofCurrentMonth.getDay();
   const lastDayofCurrentMonth = new Date(year, monthNumber + 1, 0);
-  const [daysInMonth] = useState(lastDayofCurrentMonth.getDate());
+  const daysInMonth = lastDayofCurrentMonth.getDate();
+  const days = [];
+  // grid consists of 42 days now, maybe later could check to see if this fits for the month
+  for (let i = 1; i <= 42; i++) {
+    if (i > paddingDays && i <= daysInMonth + paddingDays) {
+      days.push({
+        value: i - paddingDays,
+        date: i - paddingDays,
+      });
+    } else {
+      days.push({
+        value: "padding",
+        date: "",
+      });
+    }
+  }
 
   return (
     <div className="grid grid-cols-7 gap-4">
-      {[...Array(daysInMonth)].map((el, index) => (
-        <Day date={index + 1} key={index} />
+      {days.map((day, index) => (
+        <Day date={day.date} key={index} />
       ))}
     </div>
   );
